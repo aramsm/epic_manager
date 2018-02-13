4adad4a6c9e1b6810b20a789f4ff99baac635c35
Aram Sperduti Menocci <aramsm3@gmail.com>
2018-02-13 13:54:42 -0200

    Oops, deleted controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,15 @@
 #encoding: UTF-8
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
-    @user_identity ||= UserIdentitie.from
+    user_identity ||= UserIdentity.from_omniauth(request.env['omniauth.auth'])
+
+    if user_identity.persisted?
+      sign_in_and_redirect user_identity, notice: 'Logged in!'
+    else
+      session['devise.user_identity_attributes'] = user_identity.attributes
+      redirect_to new_user_identity_url
+    end
   end
   alias_method :facebook, :all
+  alias_method :google, :all
 end
